3f87bc782aed2bb6bb6dc79924f811b90e3452d6
godlikebangye <250138101@qq.com>
2026-08-20 18:44:38 +0800

    ci: deploy vite app to github pages

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
+  base: "/HaiBaPM/",
   optimizeDeps: {
     include: ["vue", "@arco-design/web-vue"],
   },
